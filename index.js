7dcad70658d13e599cb0eaba8665572de46f4550
Molly Lloyd <molly@mapbox.com>
2016-03-16 14:57:50 -0700

    switched to using a studio map, no local data needed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibW9sbHltZXJwIiwiYSI6ImNpbHNpZWZ3MDAwMWZ0eWtyN
 
 var map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/light-v8',
+  style: 'mapbox://styles/mollymerp/cilsjite20036wvkmxu8yu5hx',
   center: [-98.87695312499999, 40.38002840251183],
   zoom: 3,
   maxBounds: [
@@ -20,43 +20,21 @@ map.on('style.load', function() {
     url: 'mapbox://mollymerp.9kytkh5o'
   });
 
-  map.addSource('cities', {
-    type: 'geojson',
-    data: 'data/merged_msa_uac.geojson'
-  });
-
-  map.addSource('urban-areas', {
-    type: 'geojson',
-    data:'data/uac_pop_dni.geojson'
-  });
-
-  map.addLayer({
-    id: 'urban_area_polygons',
-    type: 'fill',
-    source: 'cities',
-    'source-layer': 'uac_pop_dni',
-    interactive: true,
-    paint: {
-      'fill-color': '#000000',
-      'fill-opacity': .15
-    }
-  }, 'water');
-
-  map.addLayer({
-    id: 'metro_area_polygons',
-    type:'fill',
-    source:'urban-areas',
-    'source-layer': 'merged_msa_uac',
-    interactive: true,
-    paint: {
-      'fill-color': '#000000',
-      'fill-opacity': .3
-    }
-  }, 'admin-2-boundaries')
   // add styling based on avg_solar DNI
   // legend values: [ 2.103, 2.945, 3.787, 4.629, 5.471, 6.313, 7.154, 7.996, 8.838]
+  
   var colors = ['#d73027', '#f46d43', '#fdae61', '#fee090', '#e0f3f8', '#abd9e9', '#74add1', '#4575b4'].reverse();
+  var legend = document.getElementById('legend');
+
+
+  // var colors = ['#BE171F','#D1362E','#E25040','#F06B54','#FA866A','#FFA284','#FEBEA0','#F6DBBF'].reverse();
+
   _.each(colors, function(category, i) {
+    // var legend_entry = document.createElement('div')
+    // legend_entry.className += 'legend-color pad1 col1 keyline-right';
+    // legend_entry.style.backgroundColor= category;
+    // legend.appendChild(legend_entry);
+
     map.addLayer({
       id: 'solar-' + (i + 1),
       type: 'fill',
@@ -67,7 +45,7 @@ map.on('style.load', function() {
         'fill-opacity': .7
       },
       'filter': ['==', 'category', (i + 1)]
-    }, 'urban_area_polygons');
+    }, 'water');
   });
   map.on('mousemove', _.debounce(function(e) {
     cityHover(e);
@@ -77,7 +55,7 @@ map.on('style.load', function() {
 function cityHover(e) {
   map.featuresAt(e.point, {
     radius: 10,
-    layer: 'urban_area_polygons'
+    layer: 'metro_area_polygons'
   }, function(err, features) {
     removeAllTooltips();
     if (err) {
@@ -103,7 +81,7 @@ function cityHover(e) {
   });
 }
 
-var tooltip_template = _.template("<p><strong><%= city.NAME10.replace('--', '; ') %></strong></p><p>If <%= city.NAME10.replace('--', '; ') %> had solar panels on rooftops amounting to just 1% of its total area, solar could produce<strong> <%= city.sol_1perc*100 %>%</strong> of the entire metro area's electricity needs.</p>",{
+var tooltip_template = _.template("<p><strong><%= city.NAME10.replace('--', '; ') %></strong></p><p>If <%= city.NAME10.replace('--', '; ') %> had solar panels on rooftops amounting to just 1% of its total land area (<%= Math.round((city.ALAND10_2 * .01)/1000000) %> km<sup>2</sup>), solar could produce<strong> <%= Math.round(city.sol_1perc*100) %>%</strong> of the entire metro area's electricity needs.</p>",{
     variable: 'city'
   });
 
