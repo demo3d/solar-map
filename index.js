56509709bf08f4d145afeec296d1c1edc1b28332
Molly Lloyd <molly@mapbox.com>
2016-03-15 22:23:54 -0700

    modify calculations and layers

--- index.js
+++ index.js
@@ -22,21 +22,37 @@ map.on('style.load', function() {
 
   map.addSource('cities', {
     type: 'geojson',
-    data: 'data/cities_pop_area_dni.geojson'
+    data: 'data/merged_msa_uac.geojson'
+  });
+
+  map.addSource('urban-areas', {
+    type: 'geojson',
+    data:'data/uac_pop_dni.geojson'
   });
 
   map.addLayer({
-    id: 'cities_polygons',
+    id: 'urban_area_polygons',
     type: 'fill',
     source: 'cities',
-    'source-layer': 'cities_pop_area_dni',
+    'source-layer': 'uac_pop_dni',
     interactive: true,
     paint: {
       'fill-color': '#000000',
       'fill-opacity': .15
     }
-  });
+  }, 'water');
 
+  map.addLayer({
+    id: 'metro_area_polygons',
+    type:'fill',
+    source:'urban-areas',
+    'source-layer': 'merged_msa_uac',
+    interactive: true,
+    paint: {
+      'fill-color': '#000000',
+      'fill-opacity': .3
+    }
+  }, 'admin-2-boundaries')
   // add styling based on avg_solar DNI
   // legend values: [ 2.103, 2.945, 3.787, 4.629, 5.471, 6.313, 7.154, 7.996, 8.838]
   var colors = ['#d73027', '#f46d43', '#fdae61', '#fee090', '#e0f3f8', '#abd9e9', '#74add1', '#4575b4'].reverse();
@@ -51,7 +67,7 @@ map.on('style.load', function() {
         'fill-opacity': .7
       },
       'filter': ['==', 'category', (i + 1)]
-    }, 'water');
+    }, 'urban_area_polygons');
   });
   map.on('mousemove', _.debounce(function(e) {
     cityHover(e);
@@ -61,7 +77,7 @@ map.on('style.load', function() {
 function cityHover(e) {
   map.featuresAt(e.point, {
     radius: 10,
-    layer: 'cities_polygons'
+    layer: 'urban_area_polygons'
   }, function(err, features) {
     removeAllTooltips();
     if (err) {
@@ -77,10 +93,8 @@ function cityHover(e) {
 
       // formula for % of population whose energy consumption could be offset by solar
       // ALAND10 (land area in m^2) * 0.15 * MEANANN_DN (solar potential) / DP0010001 (people) * 33 kWh/day
-
       var city = features[0].properties;
-      var percent_offset = (city.ALAND10 * 0.01 * city.MEANANN_DN) / (city.DP0010001 * 33) 
-      console.log(city, percent_offset);
+      console.log(city);
       var tooltip = new mapboxgl.Popup()
         .setLngLat(e.lngLat)
         .setHTML(tooltip_template(city))
@@ -89,7 +103,7 @@ function cityHover(e) {
   });
 }
 
-var tooltip_template = _.template("<p><strong><%= city.NAMELSAD10.replace(' Metro Area', '') %></strong></p><p>If <%= city.NAMELSAD10.replace(' Metro Area', '') %> had solar panels on rooftops amounting to just 1% of its area, solar could produce <%= Math.round((city.ALAND10 * 0.01 * city.MEANANN_DN) / (city.DP0010001 * 33)*100) %>% of the city's electricity needs.</p>",{
+var tooltip_template = _.template("<p><strong><%= city.NAME10.replace('--', '; ') %></strong></p><p>If <%= city.NAME10.replace('--', '; ') %> had solar panels on rooftops amounting to just 1% of its total area, solar could produce<strong> <%= city.sol_1perc*100 %>%</strong> of the entire metro area's electricity needs.</p>",{
     variable: 'city'
   });
 
